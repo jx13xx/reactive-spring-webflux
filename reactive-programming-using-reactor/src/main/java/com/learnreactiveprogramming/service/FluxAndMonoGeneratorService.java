a84c1dd5dbc12a2b5caf001fb54966e6fe7bec3e
jx13xx <jeenxavier23@gmail.com>
2022-07-11 10:22:05 +0400

    chore: played with inital flux code

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,57 @@
 package com.learnreactiveprogramming.service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"));
+    }
+
+
+    public Mono<String> nameMono(){
+        return Mono.just("Alex");
+    }
+
+
+    public Flux<String> nameFlux_map(){
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+                .map(String::toUpperCase);
+
+    }
+
+    public Flux<String> nameFlux_map2() {
+//        return Flux.fromIterable(List.of("alex", "ben", "chlore"))
+//                .filter(name -> name.length() >= 4);
+
+
+            return  Flux.fromIterable(List.of("alex", "ben", "chlore"))
+                    .filter(name -> name.contains("l"));
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+//        fluxAndMonoGeneratorService.namesFlux().subscribe(name -> { System.out.println(name);
+//        });
+
+//
+//        fluxAndMonoGeneratorService.nameMono().subscribe(name -> {
+//            System.out.println("Mono " +name);
+//        });
+
+        fluxAndMonoGeneratorService.nameFlux_map2().subscribe(names -> {
+            System.out.println("Filters " + names);
+        });
+
+
+    }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,36 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class FluxAndMonoGeneratorServiceTest {
+public class FluxAndMonoGeneratorServiceTest {
 
+
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+
+        // when
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        StepVerifier.create(namesFlux).expectNext("alex", "ben", "chloe")
+                .verifyComplete();
+
+        StepVerifier.create(namesFlux).expectNextCount(3).verifyComplete();
+
+
+    }
+
+    @Test
+    void namesFlux_map(){
+        var upperCaseFlux = fluxAndMonoGeneratorService.nameFlux_map();
+
+        StepVerifier.create(upperCaseFlux).expectNext("ALEX", "BEN", "CHLOE")
+                .verifyComplete();
+
+
+    }
 }
